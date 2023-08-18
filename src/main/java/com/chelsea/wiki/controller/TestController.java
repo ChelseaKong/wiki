a5afe97775fddacb7c0c7fe8a31355bd68441f53
Chelsea <ziyuan.kong@hotmail.com>
2023-08-17 21:53:20 -0700

    04 add helloInterface test test file: test.http

--- src/main/java/com/chelsea/wiki/controller/TestController.java
+++ src/main/java/com/chelsea/wiki/controller/TestController.java
@@ -13,17 +13,26 @@ public class TestController {
      * /user/1 : restful get user_id
      *
      * @RequestMapping : support all (GET, POST, PUT, DELETE)
-     * @RequestMapping(value = "/hello", method = RequestMethod.GET)
+     * @RequestMapping(value = "/hello", method = RequestMethod.GET) : support GET
      * @GetMapping : support GET
      * @PostMapping : support POST
      * @PutMapping : support PUT
      * @DeleteMapping : support DELETE
      */
-    //@RequestMapping("/hello")
+
+    // 03 add GET
+    // @RequestMapping("/hello")
     @GetMapping ("/hello")
-    // @PostMapping("/hello") status=405, request method not allowed
     public String hello() {
         return "Hello World!";
+        //http://127.0.0.1:8880/hello
+    }
+    // @PostMapping("/hello") status=405, request method not allowed
+
+    // 04 POST test
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World! POST, " + name;
         //http://127.0.0.1:8880/hello
     }
 }
