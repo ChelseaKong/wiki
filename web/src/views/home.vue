5a4687b11148d7a1aa8f56a2f2175d5362a61573
Chelsea <ziyuan.kong@hotmail.com>
2023-09-23 14:29:47 -0700

    5-09 Home shows Category Menu

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -7,52 +7,32 @@
   </div>
   -->
   <a-layout-content style="padding: 0 50px">
-    <a-breadcrumb style="margin: 16px 0">
-      <a-breadcrumb-item>Home</a-breadcrumb-item>
-    </a-breadcrumb>
     <a-layout style="padding: 24px 0; background: #fff">
       <a-layout-sider width="200" style="background: #fff">
         <a-menu
             mode="inline"
             style="height: 100%"
+            @click="handleClick"
         >
-          <!-- v-model:selectedKeys="selectedKeys2"
-          v-model:openKeys="openKeys" -->
-          <a-sub-menu key="sub1">
-            <template #title>
-                <span>
-                  <user-outlined />
-                  subnav 111111
-                </span>
+          <a-menu-item key="welcome">
+            <router-link to="'/'">
+              <MailOutLined />
+              <span> Welcome </span>
+            </router-link>
+          </a-menu-item>
+          <a-sub-menu v-for="item in level1" :key="item.id">
+            <template v-slot:title>
+              <span>
+                <user-outlined />
+                {{item.name}}
+              </span>
             </template>
-            <a-menu-item key="1">option1</a-menu-item>
-            <a-menu-item key="2">option2</a-menu-item>
-            <a-menu-item key="3">option3</a-menu-item>
-            <a-menu-item key="4">option4</a-menu-item>
-          </a-sub-menu>
-          <a-sub-menu key="sub2">
-            <template #title>
-                <span>
-                  <laptop-outlined />
-                  subnav 2
-                </span>
+            <template v-for="child in item.children" :key="child.id">
+              <MailOutlined />
+              <span>
+                {{child.name}}
+              </span>
             </template>
-            <a-menu-item key="5">option5</a-menu-item>
-            <a-menu-item key="6">option6</a-menu-item>
-            <a-menu-item key="7">option7</a-menu-item>
-            <a-menu-item key="8">option8</a-menu-item>
-          </a-sub-menu>
-          <a-sub-menu key="sub3">
-            <template #title>
-                <span>
-                  <notification-outlined />
-                  subnav 3
-                </span>
-            </template>
-            <a-menu-item key="9">option9</a-menu-item>
-            <a-menu-item key="10">option10</a-menu-item>
-            <a-menu-item key="11">option11</a-menu-item>
-            <a-menu-item key="12">option12</a-menu-item>
           </a-sub-menu>
         </a-menu>
       </a-layout-sider>
@@ -84,6 +64,8 @@
 import { defineComponent, onMounted, ref, reactive, toRef } from 'vue';
 import axios from 'axios';
 import {LikeOutlined, MessageOutlined, StarOutlined} from "@ant-design/icons-vue";
+import { message } from "ant-design-vue";
+import { Tool } from "@/util/tool";
 
 // const listData: any = [];
 // for (let i = 0; i < 23; i++) {
@@ -105,7 +87,32 @@ export default defineComponent({
     const ebooks = ref(); // ref用来定义响应式数据
     const ebooks1 = reactive({books: []}); // books是自己定义的属性，属性值用来放电子书列表
 
+    const level1 = ref();
+    let categorys: any;
+
+    // query all categorys
+    const handleQueryCategory = () => {
+      axios.get("/category/all").then( (response) => {
+        const data = response.data;
+        if (data.success) {
+          categorys = data.content;
+          console.log("Original Array: ", categorys);
+
+          level1.value = [];
+          level1.value = Tool.arrayToTree(categorys, 0);
+          console.log("Tree Structure: ", level1);
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
+    const handleClick = () => {
+      console.log("menu click");
+    };
+
     onMounted( () => { // 初始化的逻辑都写到 onMounted方法里， setup就放一些参数定义、方法定义
+      handleQueryCategory();
       //console.log("onMounted");
       axios.get("/ebook/list", {
         params: {
@@ -134,7 +141,10 @@ export default defineComponent({
       { icon: StarOutlined, text: '156' },
       { icon: LikeOutlined, text: '156' },
       { icon: MessageOutlined, text: '2' },
-    ]
+    ],
+
+      handleClick,
+      level1,
     }
   }
 });
